05e77ddcf65e186e3715a4bb2384213c15ab164b
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2022-10-13 12:01:47 -0300

    creating system

---
+++ SistemaEstacionamento/Gerenciador.cs
@@ -0,0 +1,12 @@
+using SistemaEstacionamento.Models;
+
+namespace SistemaEstacionamento
+{
+    public class Gerenciador
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
